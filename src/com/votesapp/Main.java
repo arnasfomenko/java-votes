1b53cc97d3bd42069dc2e82bf7b004d6ee52d765
Arnas <arnas@ms-7994.lan>
2018-04-09 01:44:45 +0300

    add files

---
+++ src/com/votesapp/Main.java
@@ -0,0 +1,13 @@
+package com.votesapp;
+import com.votesapp.Timestamp;
+
+import java.util.Date;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Timestamp timestamp = new Timestamp();
+        timestamp.printTime();
+    }
+}
