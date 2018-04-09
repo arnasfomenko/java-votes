2efc1354203a09f1afe5eda164a31f5655045945
RichardIgnus <rytis.bukauskas1@gmail.com>
2018-04-09 13:35:17 +0300

    StoreInfo pridetas filewriter

--- src/com/votesapp/StoreInfo.java
+++ src/com/votesapp/StoreInfo.java
@@ -1,6 +1,17 @@
 package com.votesapp;
 
+import java.io.Writer;
+
 public class StoreInfo {
 
+	private void writer() {
+		Writer wr = new FileWriter("StoreInfo.txt");
+		wr.write("Norima patalpinti informacija");
+		wr.close();
+		
+		
+	}
+	
+	
 }
 //Storint vote'am, kiekiui isaugot kiek bendrai turejo tam tikra apklausa bals�. Kad nenusiresetint� i�jungus program�
